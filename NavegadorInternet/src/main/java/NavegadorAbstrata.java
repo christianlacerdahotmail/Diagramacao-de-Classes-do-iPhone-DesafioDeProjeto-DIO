f3ec690407995591e4de6503a6060dceac6e8564
Chrístian Lacerda <christianlacerda@hotmail.com>
2024-08-29 16:52:04 -0300

    implementa navegador interface, navegador abstract

---
+++ NavegadorInternet/src/main/java/NavegadorAbstrata.java
@@ -0,0 +1,17 @@
+public abstract class NavegadorAbstrata implements NavegadorInterface {
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo a página");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando página");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página");
+    }
+}
---
+++ NavegadorInternet/src/main/java/NavegadorInterface.java
@@ -0,0 +1,5 @@
+public interface NavegadorInterface {
+    void exibirPagina();
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
